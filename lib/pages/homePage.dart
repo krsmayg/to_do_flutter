fbcb4db3a101af3dd2acf5b4180be6a7b3300f9c
Kyryll Maygur <kiryamayg@gmail.com>
2020-12-05 17:53:14 +0200

    app lifecycle

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -3,7 +3,46 @@ import 'package:hexcolor/hexcolor.dart';
 import '../widgets/task_controller.dart';
 import '../widgets/reusableWidgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+    print('***init state***');
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+    print('***dispose***');
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print('state = ${state.toString()}');
+
+    switch (state) {
+      case AppLifecycleState.inactive:
+        print('***inactive***');
+        break;
+      case AppLifecycleState.paused:
+        print('***paused***');
+        break;
+      case AppLifecycleState.resumed:
+        print('***resumed***');
+        break;
+      case AppLifecycleState.detached:
+        print('***detached***');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
